f38f341422d3a3497957f1d7d9008a9f4aab0254
erenkaraboga <eren.karaboga@naylalabs.com>
2023-10-19 11:26:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stocklistingapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.example.stocklistingapp.data.mapper
+
+import com.example.stocklistingapp.data.local.CompanyListingEntity
+import com.example.stocklistingapp.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing{
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity{
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
---
+++ app/src/main/java/com/example/stocklistingapp/domain/model/CompanyListing.kt
@@ -0,0 +1,8 @@
+package com.example.stocklistingapp.domain.model
+
+
+data class CompanyListing(
+    val name: String,
+    val symbol: String,
+    val exchange: String,
+)
