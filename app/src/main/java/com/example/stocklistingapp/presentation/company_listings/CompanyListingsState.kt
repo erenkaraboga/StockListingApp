1e16176cbd9bfac314575de6f048799e6aec9f14
erenkaraboga <eren.karaboga@naylalabs.com>
2023-10-21 22:27:24 +0300

    Add company listing event

---
+++ app/src/main/java/com/example/stocklistingapp/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.example.stocklistingapp.presentation.company_listings
+
+import com.example.stocklistingapp.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
