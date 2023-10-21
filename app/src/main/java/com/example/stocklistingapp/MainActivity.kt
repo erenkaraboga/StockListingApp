eaec43fa792e51b51dad5b406b5e9c1f164764d1
erenkaraboga <eren.karaboga@naylalabs.com>
2023-10-21 22:20:15 +0300

    Fix gradle issues

--- app/src/main/java/com/example/stocklistingapp/MainActivity.kt
+++ app/src/main/java/com/example/stocklistingapp/MainActivity.kt
@@ -7,17 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.stocklistingapp.ui.theme.StockListingAppTheme
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StockListingAppTheme {
+            MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -38,7 +38,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    StockListingAppTheme {
+    MyApplicationTheme {
         Greeting("Android")
     }
 }
