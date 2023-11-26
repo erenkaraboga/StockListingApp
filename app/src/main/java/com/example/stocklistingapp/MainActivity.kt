145adf275fff26717a18eea2f9d93e0ac30ce599
erenkaraboga <eren.karaboga@naylalabs.com>
2023-11-27 00:21:32 +0300

    Add splash screen

--- app/src/main/java/com/example/stocklistingapp/MainActivity.kt
+++ app/src/main/java/com/example/stocklistingapp/MainActivity.kt
@@ -7,17 +7,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavGraph
 import com.example.stocklistingapp.presentation.NavGraphs
 import com.example.stocklistingapp.ui.theme.MyApplicationTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MyApplicationTheme {
                 Surface(
